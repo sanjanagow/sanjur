a6980b1f4764068c4f495956b750c067e7a1602b
sanjanagow <160707554+sanjanagow@users.noreply.github.com>
2024-02-28 12:00:32 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello{
 	public static void main(String args[])
 	{
 		system.out.println("hello")
-		System.ouit.println("hello tumkur");
-		System.out.println("spt");
+		System.ouit.println("hello kunigal");
+		System.out.println("spt tumkur");
 	}
 }
